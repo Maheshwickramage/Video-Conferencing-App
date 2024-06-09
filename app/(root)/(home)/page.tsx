951c260b5651a8136e86de05aa2ac50f9515e44b
Darshana Mahesh <139673873+Maheshwickramage@users.noreply.github.com>
2024-06-09 22:39:01 +0530

    updated-authentication

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <section className='flex size-full flex-col gap-10 text-white'>
+    <section className='flex size-full  flex-col gap-10 text-white'>
 
       <div className='text-3xl font-bold' >
         <h1>
